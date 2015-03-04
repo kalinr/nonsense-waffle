37f107956e0c1c3542e5c4122c22ac951054dd1b
Kalin Ringkvist <kalin@kalinflash.com>
2015-03-04 12:30:11 -0800

    got basic word saving into a collection with word (which is also used as its id), word type and description from a basic admin form

---
+++ nonsense-waffle/nonsense-waffle.js
@@ -0,0 +1,54 @@
+
+
+if (Meteor.isClient) {
+
+  Template.adminPage.events({
+
+    //-------------BEGIN FORM CHANGE EVENTS-----------------
+    'change [type=radio]': function(e,t){
+      Session.set("sType", e.target.value);
+    },
+    'change #txtWordDescription': function(evt){
+      Session.set("sDescription", evt.target.value);
+    },
+    'change #txtNewWord': function(evt){
+      Session.set("sNewWord", evt.target.value);
+    },
+    //-------------END FORM CHANGE EVENTS-----------------
+
+    //submit form action covers both the button click and enter button
+    'submit form': function(evt){
+      evt.preventDefault();
+
+      //insert the new word into the database, using the word itself as its unique ID
+      colWords.insert({
+        _id: Session.get("sNewWord"),
+        description: Session.get("sDescription"),
+        type: Session.get("sType"),
+        dateAdded: Date.now()
+      });
+    }
+  });
+
+  Template.adminPage.helpers({
+    words: function() {
+      return colWords.find();
+    },
+    sDescription: function(){
+      return Session.get("sDescription");
+    },
+    sNewWord: function(){
+      return Session.get("sNewWord");
+    },
+    sType: function(){
+      return Session.get("sType");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    //remove all records for testing purposes
+    colWords.remove({});
+  });
+}
