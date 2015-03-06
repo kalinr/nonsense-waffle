3c1ce602c7046ec89dea19e403d710f6bbcf79d0
Kalin Ringkvist <kalin@kalinflash.com>
2015-03-05 23:11:51 -0800

    added a feature to the admin page to import some text files I created from copying and pasting the output of the old google app engine version of this game, so we can now easily get all the old words into the game by pasting them into box on the admin page

--- nonsense-waffle/client/views/admin.js
+++ nonsense-waffle/client/views/admin.js
@@ -6,28 +6,83 @@ if (Meteor.isClient) {
 
     //-------------BEGIN FORM CHANGE EVENTS-----------------
     'change [type=radio]': function(e,t){
-      Session.set("sType", e.target.value);
+      Session.set('sType', e.target.value);
     },
     'change #txtWordDescription': function(evt){
-      Session.set("sDescription", evt.target.value);
+      Session.set('sDescription', evt.target.value);
     },
     'change #txtNewWord': function(evt){
-      Session.set("sNewWord", evt.target.value);
+      Session.set('sNewWord', evt.target.value);
+    },
+    'change #txtImport': function(evt){
+      Session.set('sImport', evt.target.value);
+    },
+    'click #btnClearWords': function(evt){
+      /*
+      //Okay, this doesn't work because this client-side code isn't trusted. Doing this manually in server.js instead
+      if(confirm("Are you sure you want to delete all records from the word database. Please don't do this unless you know what you're doing. All the words will be gone from the game if you do.")) {
+        colWords.remove({});
+      }*/
     },
     //-------------END FORM CHANGE EVENTS-----------------
 
     //submit form action covers both the button click and enter button
-    'submit form': function(evt){
+    'submit #formNewWord': function(evt){
       evt.preventDefault();
 
       //insert the new word into the database, using the word itself as its unique ID
       colWords.insert({
-        _id: Session.get("sNewWord"),
-        description: Session.get("sDescription"),
-        type: Session.get("sType"),
+        _id: Session.get('sNewWord'),
+        description: Session.get('sDescription'),
+        type: Session.get('sType'),
         dateAdded: Date.now()
       });
+
+      Session.set('sType', '');
+      Session.set('sDescription', '');
+      Session.set('sNewWord', '');
+    },
+
+    //this takes one of three files that I created by copying and pasting the google app engine datastore into txt files in the dev directory
+    //adjectivesExport.txt, nounExport.txt and verbExport.txt (I had three separate tables in the app engine version)
+    //copy/paste the entire textfile into the textfield at the bottom of the admin page and hit submit to convert it into
+    //the words mongodb collection
+    'submit #formImport': function(evt){
+      evt.preventDefault();
+
+      var sData = Session.get('sImport'),
+        sType = sData.substr(0, 4),//grab the type from the top of the file
+        aWords = sData.split(' *+'),//I put a " *+' between each of them
+        aDocuments = [],
+        nCount = 0;
+
+      if(sType === 'adje'){//complete the word 'adjective' not necessary for 'noun' and 'verb' because those are already 4 characters
+        sType = 'adjective';
+      }
+
+      for(var i=0; i<aWords.length; i++){
+        switch(i % 4){//each entry had four pieces of data.
+          case 0://ignore the old id
+          case 1://ignore the old date
+            continue;
+            break;
+          case 2:
+            //create the new object, with our type and set the date to now()
+            aDocuments[nCount] = {type:sType, description:aWords[i], dateAdded:Date.now()};
+            break;
+          case 3:
+            aDocuments[nCount]._id = aWords[i];
+
+            //not sure why inserting the whole array at once is not working so we just do it on this loop instead
+            colWords.insert(aDocuments[nCount]);
+            nCount++;
+            break;
+        }
+      }
+
+      Session.set('sImport', '');
     }
+
   });
 
   Template.admin.helpers({
@@ -35,13 +90,16 @@ if (Meteor.isClient) {
       return colWords.find();
     },
     sDescription: function(){
-      return Session.get("sDescription");
+      return Session.get('sDescription');
     },
     sNewWord: function(){
-      return Session.get("sNewWord");
+      return Session.get('sNewWord');
     },
     sType: function(){
-      return Session.get("sType");
+      return Session.get('sType');
+    },
+    sImport: function(){
+      return Session.get('sImport');
     }
   });
 }
