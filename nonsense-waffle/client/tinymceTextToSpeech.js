2e6b4261453efea8eafe8e2a83e3e30d9a8c2708
Kalin Ringkvist <kalin@kalinflash.com>
2015-03-26 00:58:29 -0700

    Got speechToText all set up and theoretically it should all work but now transferring back to my laptop for actual testing

--- nonsense-waffle/client/tinymceTextToSpeech.js
+++ nonsense-waffle/client/tinymceTextToSpeech.js
@@ -1,123 +1,149 @@
 tinymceTextToSpeechInit = function () {
 
-  tinymce.PluginManager.add('texttospeech', function (editor, url) {
-    //we maintain our own currentlyReading flag instead of going off speechmanager.bSpeaking to avoid confusion if
-    // speechmanager is speaking from somewhere else in the app since you can re-use speechmanager other places
-    var bCurrentlyReading = false;
+  if (SpeechManager.bSpeechSupported) {
+    tinymce.PluginManager.add('texttospeech', function (editor, url) {
+      //we maintain our own currentlyReading flag instead of going off speechmanager.bSpeaking to avoid confusion if
+      // speechmanager is speaking from somewhere else in the app since you can re-use speechmanager other places
+      var bCurrentlyReading = false;
 
-    SpeechManager.on('speechStopped', function () {
-      bCurrentlyReading = false;
-      editor.fire('ReadToMeStateChanged', {state: false});
-    });
+      SpeechManager.on('speakingStopped', function () {
+        bCurrentlyReading = false;
+        editor.fire('ReadToMeStateChanged', {state: false});
+      });
 
-    //starts reading the current text aloud. If currently reading, it stops it, so this is kind of a toggle function
-    function readToMe() {
+      //starts reading the current text aloud. If currently reading, it stops it, so this is kind of a toggle function
+      function readToMe() {
 
-      //if it's currently speaking, just stop
-      if (bCurrentlyReading) {
-        SpeechManager.stopSpeaking();
-      } else {
-        SpeechManager.speak(tinymce.activeEditor.getContent());
+        //if it's currently speaking, just stop
+        if (bCurrentlyReading) {
+          SpeechManager.stopSpeaking();
+          bCurrentlyReading = false;
+        } else {
+          SpeechManager.speak(tinymce.activeEditor.getContent());
+          bCurrentlyReading = true;
+
+          //tell the button and menu item to update their state
+          editor.fire('ReadToMeStateChanged', {state: bCurrentlyReading});
+        }
       }
 
-      bCurrentlyReading = !bCurrentlyReading;
+      //Set up the button in the toolbar
+      editor.addButton('texttospeech', {
+        text: 'Read',
+        tooltip: 'Read aloud your current content using the robot voice',
+        icon: false, //icon: 'my_icon',
+        onclick: readToMe,
 
-      //tell the button and menu item to update their state
-      editor.fire('ReadToMeStateChanged', {state: bCurrentlyReading});
-    }
+        onPostRender: function () {
+          var self = this;
 
-    //Set up the button in the toolbar
-    editor.addButton('texttospeech', {
-      text: 'Read',
-      tooltip: 'Read aloud your current content',
-      icon: false, //icon: 'my_icon',
-      onclick: readToMe,
+          editor.on('ReadToMeStateChanged', function (e) {
+            self.active(e.state);
+          });
+        }
+      });
 
-      onPostRender: function () {
-        var self = this;
+      // Adds a menu item to the tools menu
+      editor.addMenuItem('texttospeech', {
+        text: 'Read to me',
+        context: 'tools',
+        onclick: readToMe,
 
-        editor.on('ReadToMeStateChanged', function(e) {
-          self.active(e.state);
-        });
-      }
+        onPostRender: function () {
+          var self = this;
+
+          editor.on('ReadToMeStateChanged', function (e) {
+            self.active(e.state);
+          });
+        }
+
+      });
     });
+  }
 
-    // Adds a menu item to the tools menu
-    editor.addMenuItem('texttospeech', {
-      text: 'Read aloud',
-      context: 'tools',
-      onclick: readToMe,
+  if (SpeechManager.bListenSupported) {
+    tinymce.PluginManager.add('speechtotext', function (editor, url) {
+      var bCurrentlyListening = false;
 
-      onPostRender: function() {
-        var self = this;
+      SpeechManager.on('listenResult', function (evt) {
+        var sCurContent = tinymce.activeEditor.getContent(),
+        //nBeginIndex = sCurContent.indexOf('<speechcontent>'),
+          aCurContent = sCurContent.split('<!--speech-->');
 
-        editor.on('ReadToMeStateChanged', function(e) {
-          self.active(e.state);
-        });
-      }
+        if (aCurContent.length === 3) {
 
-    });
-  });
+          if (evt.isFinal) {
+            tinymce.activeEditor.setContent(aCurContent[0] + evt.sNewText + aCurContent[2]);
+          } else {
+            tinymce.activeEditor.setContent(aCurContent[0] + '<!--speech-->' + evt.sNewText + '<!--speech-->' + aCurContent[2]);
+          }
+        } else {
 
-  tinymce.PluginManager.add('speechtotext', function (editor, url) {
+          if (aCurContent.length !== 1) {
+            throw new Error("Somehow we got the wrong number of <!--speech--> tags");
+          }
 
-    var bCurrentlyListening = false;
+          tinymce.execCommand('mceInsertContent', false, '<!--speech-->' + evt.sNewText + '<!--speech-->');
+        }
 
-    SpeechManager.on('listeningStopped', function () {
-      bCurrentlyListening = false;
-      editor.fire('ListenToMeStateChanged', {state: false});
-    });
+        //we put this if statement down here just in case the final event is also the first
+        if (evt.isFinal) {
+          bCurrentlyListening = false;
+          SpeechManager.stopListening();
+          //editor.fire('ListenToMeStateChanged', {state: false});
+        }
+      });
 
-    //starts listening to the user
-    function listenToMe() {
-      //if it's currently speaking, just stop
-      if (bCurrentlyListening) {
-        SpeechManager.stopListening();
-      } else {
-        //SpeechManager.speak(tinymce.activeEditor.getContent());
-        console.log("inserting text!!!");
-        //tinyMCE.execCommand('mceInsertContent',false, 'blabla this is tekst');
+      SpeechManager.on('listeningStopped', function (evt) {
+        bCurrentlyListening = false;
+        editor.fire('ListenToMeStateChanged', {state: false});
+      });
 
-        SpeechManager.listen();
-      }
-
-      bCurrentlyListening = !bCurrentlyListening;
-
-      //tell the button and menu item to update their state
-      editor.fire('ListenToMeStateChanged', {state: bCurrentlyListening});
-    }
-
-
-    //Set up the button in the toolbar
-    editor.addButton('speechtotext', {
-      text: 'Listen',
-      tooltip: 'Talk to add text. Make sure your mic is on and click the "allow" button at the top of the browser.',
-      icon: false, //icon: 'my_icon',
-      onclick: listenToMe,
-
-      onPostRender: function () {
-        var self = this;
-
-        editor.on('ListenToMeStateChanged', function (e) {
-          self.active(e.state);
-        });
-      }
-    });
-
-    // Adds a menu item to the tools menu
-    editor.addMenuItem('speechtotext', {
-      text: 'Listen to me',
-      context: 'tools',
-      onclick: listenToMe,
-
-      onPostRender: function() {
-        var self = this;
-
-        editor.on('ListenToMeStateChanged', function (e) {
-          self.active(e.state);
-        });
+      //starts listening to the user
+      function listenToMe() {
+        //if it's currently speaking, just stop
+        if (bCurrentlyListening) {
+          SpeechManager.stopListening();
+          bCurrentlyListening = false;
+        } else {
+          SpeechManager.listen();
+          bCurrentlyListening = true;
+          //tell the button and menu item to update their state
+          editor.fire('ListenToMeStateChanged', {state: bCurrentlyListening});
+        }
       }
 
+      //Set up the button in the toolbar
+      editor.addButton('speechtotext', {
+        text: 'Listen',
+        tooltip: 'Talk to add text. Make sure your mic is on and click the "allow" button at the top of the browser.',
+        icon: false, //icon: 'my_icon',
+        onclick: listenToMe,
+
+        onPostRender: function () {
+          var self = this;
+
+          editor.on('ListenToMeStateChanged', function (e) {
+            self.active(e.state);
+          });
+        }
+      });
+
+      // Adds a menu item to the tools menu
+      editor.addMenuItem('speechtotext', {
+        text: 'Listen to me',
+        context: 'tools',
+        onclick: listenToMe,
+
+        onPostRender: function () {
+          var self = this;
+
+          editor.on('ListenToMeStateChanged', function (e) {
+            self.active(e.state);
+          });
+        }
+
+      });
     });
-  });
+  }
 }
--- nonsense-waffle/packages/speechmanager/speechmanager.js
+++ nonsense-waffle/packages/speechmanager/speechmanager.js
@@ -27,20 +27,23 @@ SpeechManager = {
     if ('webkitSpeechRecognition' in window) {//this browser can listen
       this.bListenSupported = true;
 
-      console.log("speechsupported!!!!!");
-
-
       this.oSpeechRecognition = new webkitSpeechRecognition();
       this.oSpeechRecognition.continuous = true;
       this.oSpeechRecognition.interimResults = true;
-      this.oSpeechRecognition.onresult = function(event) {
-        console.log("speechrecog event", event)
-      }
+
+      this.oSpeechRecognition.onresult = this.onListenResult;
 
       console.log(this.oSpeechRecognition);
     }
   },
 
+  saveState: function () {
+    if (Storage !== undefined) {
+      localStorage.setItem('bSpeechEnabled', this.bSpeechEnabled);
+    }
+  },
+
+  //-----------------------Begin Speak functions--------------------------------
   speak: function (msg) {
     var elTemp,
       sentences,
@@ -48,7 +51,7 @@ SpeechManager = {
       self = this;
 
     if (this.bSpeechEnabled && this.bSpeechSupported) {
-
+      this.stopListening();
       this.bSpeaking = true;
 
       //get the browser to strip out all the html from the msg
@@ -79,23 +82,52 @@ SpeechManager = {
   stopSpeaking: function () {
     speechSynthesis.cancel();
     this.bSpeaking = false;
-    this.trigger('speechStopped');
+    this.trigger('speakingStopped');
   },
+  //-----------------------End Speak functions--------------------------------
 
+  //-----------------------Begin Listen functions--------------------------------
   listen: function () {
-
-    console.log("starting to listen!!!");
-
+    this.stopSpeaking();
     this.oSpeechRecognition.start();
   },
 
-  stopListening: function () {
+  onListenResult: function(evt) {
+    var i,
+      sNewText = "",
+      isFinal = false;
 
-    console.log(this.oSpeechRecognition.stop);
+    console.log(evt);
 
-    this.oSpeechRecognition.stop();
+    //loop through all results and concatenate them together (usually there's only one but if the API is sure about
+    //the first one and still thinking about the second one, it gives us a second so we add that to the string
+    for (i = 0; i < evt.results.length; i++) {
+      //take only the first option from each result as that is supposed to be the most likely to be correct
+      sNewText += evt.results[i][0].transcript;
+
+      //if anything is considered final, set it as final (I have no idea if this is necessary because if it's final
+      //it should just give one result, but I'm not sure if that will always be the case)
+      if (evt.results[i][0].isFinal) {
+        isFinal = true;
+      }
+    }
+    this.trigger('listenResult', {sNewText: sNewText, isFinal: isFinal});
+
+    //once isFinal is true, Google Chrome just stops accepting voice input so we have no choice but to abandon ship
+    //the user has to stop talking for a few seconds to get it to send isFinal as true
+    if (isFinal) {
+      this.stopListening();
+    }
   },
 
+  stopListening: function () {
+    this.oSpeechRecognition.stop();
+    this.bSpeaking = false;
+    this.trigger('listeningStopped');
+  },
+  //-----------------------End listening functions--------------------------------
+
+  //-----------------------Begin event handling--------------------------------
   //add an event listener
   on: function(event, callback) {
     if (!this.oTriggers[event]) {
@@ -113,13 +145,9 @@ SpeechManager = {
       }
     }
   },
+  //-----------------------End event handling--------------------------------
 
-  saveState: function () {
-    if (Storage !== undefined) {
-      localStorage.setItem('bSpeechEnabled', this.bSpeechEnabled);
-    }
-  },
-
+  //-----------------------Begin enable/disable--------------------------------
   disable: function () {
     this.bSpeechEnabled = false;
     if (this.bSpeechSupported) {
@@ -141,4 +169,6 @@ SpeechManager = {
     }
     return this.bSpeechEnabled;
   }
+  //-----------------------End enable/disable--------------------------------
+
 }
